4ad98a1fa18a2aaa5434f56505d92bac3a9452eb
michelyyeni <michelyyeni@192.168.1.40>
2013-01-15 16:52:21 +0100

    Commit 4 prueba control versiones

--- src/es/aguasnegras/pruebagit/PruebaGit.java
+++ src/es/aguasnegras/pruebagit/PruebaGit.java
@@ -9,6 +9,7 @@ public class PruebaGit {
 		// TODO Auto-generated method stub
 		System.out.println("Subversion, tu antes molabas");
 		//esto es una prueba de control de versiones
+		//Ahora realizo una prueba el 15-01-2013 a las 16:48
 	}
 
 }
